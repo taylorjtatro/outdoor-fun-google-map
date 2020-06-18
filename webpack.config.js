b5edb70af3dd45f614b9b5e18ec0c3fe273e0f6f
taylorjtatro <taylorjtatro@gmail.com>
2020-06-17 20:33:36 -0700

    still failing

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const api_key = process.env.GOOGLEMAPS_KEY;
+const webpack = require('webpack');
 module.exports = {
     entry: ['./src/js/index.js'],
     output: {
@@ -19,7 +20,20 @@ module.exports = {
             apiUrl: `https://maps.googleapis.com/maps/api/js?key=${api_key}&callback=initMap"
             type="text/javascript`
         }),
-        new Dotenv()
+        new Dotenv({
+            path: './.env'
+        }),
+        new webpack.DefinePlugin({
+            PRODUCTION: JSON.stringify(true),
+            VERSION: JSON.stringify('5fa3b9'), 
+            BROWSER_SUPPORTS_HTML5: true,
+            TWO: '1+1',
+            'typeof window': JSON.stringify('object'),
+            'process.env': {
+              NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            },
+            api_key = process.env.GOOGLEMAPS_KEY
+          })
     ],
     module: { 
         rules: [
